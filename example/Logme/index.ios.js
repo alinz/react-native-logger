563c59dcb073256e34282dffe478ccaec03b50fa
Artem Russkikh <rusartx@gmail.com>
2016-10-10 02:23:23 +0500

    Update example

--- example/Logme/index.ios.js
+++ example/Logme/index.ios.js
@@ -1,12 +1,57 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
+ * @flow
  */
-'use strict';
-import React, {
-  AppRegistry
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
 } from 'react-native';
 
-import App from './app';
+import log from './logger';
 
-AppRegistry.registerComponent('Logme', () => App);
+log(View);
+
+class Logme extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('Logme', () => Logme);
--- example/Logme/logger.js
+++ example/Logme/logger.js
@@ -4,7 +4,7 @@ const noop = () => {};
 
 const initLogger = () => {
   if (process.env.NODE_ENV !== 'production') {
-    return logger('localhost') //for android make sure you use your ip addtess e.g '192.168.1.110'
+    return logger();
   }
   return noop;
 }
