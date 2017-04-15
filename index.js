5a598e819d6aaf6cf706e162ff2d9a7b117a3380
Ali Najafizadeh <a.najafizadeh@gmail.com>
2017-04-15 17:42:19 -0400

    fixed an issue related to production build

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 type groupLogFn = (...args: Array<any>) => void
 
-var isDebugEnabled = global && typeof global.__DEV__ !== 'undefined' && !!global.__DEV__
+const isDebugEnabled = global && typeof global.__DEV__ !== 'undefined' && !!global.__DEV__
 
 const noop = () => {}
 const displayLogs = (name: string, title: string, logs: Array<Array<any>>, show: boolean) => {
@@ -26,7 +26,6 @@ const displayLogs = (name: string, title: string, logs: Array<Array<any>>, show:
 let filters: Array<string> = []
 
 export const logger = {
-  enable: (value: boolean) => isDebugEnabled = value,
   setFilters: (...names: Array<string>): void => {
     filters = names
   },
@@ -34,25 +33,23 @@ export const logger = {
     isDebugEnabled && console.log(...args)
   },
   group: (name: string, title: string, logFn: (log: groupLogFn) => void | Promise<any> = noop) => {
-    if (isDebugEnabled) {
-      const show = filters.length === 0 || filters.indexOf(name) !== -1
+    const show = filters.length === 0 || filters.indexOf(name) !== -1
 
-      const logs: Array<any> = []
-      const done = logFn((...args: Array<any>): void => {
-        if (show) {
-          logs.push(args)
-        }
-      })
-
-      if (done instanceof Promise) {
-        done.then(() => {
-          displayLogs(name, title, logs, show)
-        }).catch(() => {
-          displayLogs(name, title, logs, show)
-        })
-      } else {
-        displayLogs(name, title, logs, show)
+    const logs: Array<any> = []
+    const done = logFn((...args: Array<any>): void => {
+      if (isDebugEnabled && show) {
+        logs.push(args)
       }
+    })
+
+    if (done instanceof Promise) {
+      done.then(() => {
+        displayLogs(name, title, logs, show)
+      }).catch(() => {
+        displayLogs(name, title, logs, show)
+      })
+    } else {
+      displayLogs(name, title, logs, show)
     }
   }
 }
